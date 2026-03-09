711c9e31e934ea33c12256066bc170d3d3e8f0c3
vijay0262-alt <vijay0262@gmail.com>
2026-03-09 22:45:15 +0530

    Update Jivochat.tsx

--- components/Jivochat.tsx
+++ components/Jivochat.tsx
@@ -4,7 +4,7 @@ import Script from "next/script"
 
 export default function JivoChat() {
   return (
-        <script src="//code.jivosite.com/widget/vGygZb1Lna" strategy="lazyOnload" async></script>
+        <script src="//code.jivosite.com/widget/vGygZb1Lna" async></script>
 
   )
 }
