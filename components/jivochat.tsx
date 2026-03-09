01f3a95364bb69ed88d176d759a9fd3da795803c
vijay0262-alt <vijay0262@gmail.com>
2026-03-09 22:36:49 +0530

    Create jivochat.tsx

---
+++ components/jivochat.tsx
@@ -0,0 +1,10 @@
+"use client"
+
+import Script from "next/script"
+
+export default function JivoChat() {
+  return (
+        <script src="//code.jivosite.com/widget/vGygZb1Lna" strategy="lazyOnload" async></script>
+
+  )
+}
