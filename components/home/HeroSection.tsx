93484cad3fc2e06b5052f435db1d2bd6c218b2cc
imvijaymehra-a11y <imvijaymehra@gmail.com>
2026-06-02 00:56:57 +0530

    Update HeroSection.tsx

--- components/home/HeroSection.tsx
+++ components/home/HeroSection.tsx
@@ -77,8 +77,8 @@ export default function HeroSection() {
           <Image
             src="/hero.png"
             alt="Security Camera and Smart Doorbell Installation"
-            width={700}
-            height={600}
+            width={500}
+            height={400}
             className="rounded-2xl shadow-2xl"
           />
           {/* Stat overlay on image */}
